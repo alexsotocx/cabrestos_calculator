b42313997814e18b20f2a0126964f78f411c872c
Alexander Soto <asoto@minddoc.de>
2020-02-26 21:56:29 +0100

    Cabrestos calculator

--- src/util/cabrestosHelper.ts
+++ src/util/cabrestosHelper.ts
@@ -2,8 +2,8 @@ export class CabrestosHelper {
   constructor(
     readonly flagSupportLengthMeters: number,
     readonly lineSeparationCentimeters: number,
-    readonly centerLineLengthMeters: number,
-  ) { }
+    readonly centerLineLengthMeters: number
+  ) {}
 
   validateInput(): boolean {
     return this.flagSupportCM() % this.lineSeparationCentimeters === 0;
@@ -13,15 +13,42 @@ export class CabrestosHelper {
     if (this.validateInput()) {
       return this;
     }
-    const validFlagSLengthCM = Math.ceil(this.flagSupportCM() / this.lineSeparationCentimeters) * this.lineSeparationCentimeters;
+    const validFlagSLengthCM =
+      Math.ceil(this.flagSupportCM() / this.lineSeparationCentimeters) *
+      this.lineSeparationCentimeters;
     return new CabrestosHelper(
       validFlagSLengthCM / 100,
       this.lineSeparationCentimeters,
-      this.centerLineLengthMeters,
+      this.centerLineLengthMeters
     );
+  }
+
+  calculateCabrestos(): number[] {
+    if (!this.validateInput()) {
+      throw new Error("Invalid Configuration");
+    }
+    const output: number[] = [];
+    const distanceCMToCenter = this.flagSupportCM() / 2;
+    const cabrestosTotal =
+      this.flagSupportCM() / this.lineSeparationCentimeters + 1;
+    for (let i = 0; i < cabrestosTotal; i++) {
+      const length = Math.abs(
+        distanceCMToCenter - i * this.lineSeparationCentimeters
+      );
+      const distance = this.distancePit(
+        length,
+        this.centerLineLengthMeters * 100
+      );
+      output.push(Number((distance / 100).toFixed(2)));
+    }
+    return output;
   }
 
   private flagSupportCM() {
     return this.flagSupportLengthMeters * 100;
+  }
+
+  private distancePit(length: number, high: number): number {
+    return Math.sqrt(length * length + high * high);
   }
 }
--- tests/unit/cabrestrosHelper.spec.ts
+++ tests/unit/cabrestrosHelper.spec.ts
@@ -1,6 +1,6 @@
-import { CabrestosHelper } from '@/util/cabrestosHelper';
+import { CabrestosHelper } from "@/util/cabrestosHelper";
 
-describe('CabrestosHelper', () => {
+describe("CabrestosHelper", () => {
   const flagLengthMeters = 1;
   const cLineMeters = 10;
   const notDivisor = 30;
@@ -9,39 +9,58 @@ describe('CabrestosHelper', () => {
   const invalidInput = new CabrestosHelper(
     flagLengthMeters,
     notDivisor,
-    cLineMeters,
+    cLineMeters
   );
   const validInput = new CabrestosHelper(
     flagLengthMeters,
     divisor,
-    cLineMeters,
+    cLineMeters
   );
 
-  describe('validateInput', () => {
-
-    test('false when the separation is not a divisor of the full length', () => {
+  describe("validateInput", () => {
+    test("false when the separation is not a divisor of the full length", () => {
       expect(invalidInput.validateInput()).toBeFalsy();
     });
 
-    test('true when the separation is a divisor of the full length', () => {
+    test("true when the separation is a divisor of the full length", () => {
       expect(validInput.validateInput()).toBeTruthy();
     });
   });
 
-  describe('generateSuggestionInput', () => {
-    test('returns the closest valid input for the given configuration', () => {
+  describe("generateSuggestionInput", () => {
+    test("returns the closest valid input for the given configuration", () => {
       const suggestion = invalidInput.generateSuggestion();
       expect(suggestion).not.toBe(invalidInput);
       expect(suggestion.validateInput()).toEqual(true);
       expect(suggestion.flagSupportLengthMeters).toEqual(1.2);
       expect(suggestion.centerLineLengthMeters).toEqual(cLineMeters);
       expect(suggestion.lineSeparationCentimeters).toEqual(notDivisor);
-
     });
 
-    test('if configuration is already valid returns itself', () => {
+    test("if configuration is already valid returns itself", () => {
       const suggestion = validInput.generateSuggestion();
       expect(suggestion).toBe(validInput);
+    });
+  });
+
+  describe("calculateCabrestos", () => {
+    const evenConfiguration = new CabrestosHelper(1, 20, 5);
+    const oddConfiguration = new CabrestosHelper(1, 25, 5);
+
+    test("raises exception if configuration is not valid", () => {
+      expect(() => invalidInput.calculateCabrestos()).toThrow();
+    });
+
+    test("with even configuration", () => {
+      const cal = evenConfiguration.calculateCabrestos();
+      expect(cal.length).toEqual(6);
+      expect(cal).toEqual([5.02, 5.01, 5.0, 5.0, 5.01, 5.02]);
+    });
+
+    test("with odd configuration", () => {
+      const cal = oddConfiguration.calculateCabrestos();
+      expect(cal.length).toEqual(5);
+      expect(cal).toEqual([5.02, 5.01, 5.0, 5.01, 5.02]);
     });
   });
 });
